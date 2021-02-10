2af6756c72a8439ac723e553c8dfd49ebc3eeb69
Chuck Mogayzel <mogayzelc@gmail.com>
2021-02-10 12:40:17 -0800

    getting error from the webpack-dev-server

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
+    mode: 'production',
+    devtool: 'source-map',
     module: {
         rules: [{
             test: '/\.js$/',
