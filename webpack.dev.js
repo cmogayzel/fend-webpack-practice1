da68a84c4eed7383072302686e1ea7c42018cb4c
Chuck Mogayzel <mogayzelc@gmail.com>
2021-02-10 22:11:08 -0800

    Added plugin and tried install webpack-dev-server again no go.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     module: {
         rules: [{
             test: '/\.js$/',
@@ -16,7 +21,18 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
-    ]
+
+    ],
+
 
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     devtool: 'source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     module: {
         rules: [{
             test: '/\.js$/',
@@ -16,7 +21,16 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
-    ]
+    ],
 
 }
